fcf49aa5892c69bfe6d99c6c72473e2b639e4303
Andrii Bessarab <andriibessarab@gmail.com>
2024-12-08 21:01:36 -0500

    Create SwerveSubsystem

---
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.io.File;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.math.util.Units;
+
+import swervelib.parser.SwerveParser;
+import swervelib.SwerveDrive;
+
+public class SwerveSubsystem extends SubsystemBase {
+  double maximumSpeed = Units.feetToMeters(4.5);
+  File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
+  SwerveDrive swerveDrive;
+
+  /** Creates a new SwerveSubsystem. */
+  public SwerveSubsystem() {
+    try {
+      swerveDrive = new SwerveParser(swerveJsonDirectory).createSwerveDrive(maximumSpeed);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
