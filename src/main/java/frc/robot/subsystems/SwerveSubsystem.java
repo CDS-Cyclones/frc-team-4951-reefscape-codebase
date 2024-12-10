a073d227584828122af0baeab198646e3ea89460
Andrii Bessarab <andriibessarab@gmail.com>
2024-12-09 20:14:34 -0500

    Add ability to tune drive & angle PIDs through SmartDashboard

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -11,14 +11,21 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
+import swervelib.parser.PIDFConfig;
 import swervelib.parser.SwerveParser;
 import swervelib.SwerveDrive;
 
 public class SwerveSubsystem extends SubsystemBase {
   File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
   SwerveDrive swerveDrive;
+
+  // PID NetworkTableEntries
+  private NetworkTableEntry driveP, driveI, driveD, turnP, turnI, turnD;
 
   /** Creates a new SwerveSubsystem. */
   public SwerveSubsystem() {
@@ -27,6 +34,22 @@ public class SwerveSubsystem extends SubsystemBase {
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
+
+    // Initialize NetworkTableEntries
+    driveP = NetworkTableInstance.getDefault().getTable("SmartDashboard").getEntry("Drive PID P");
+    driveI = NetworkTableInstance.getDefault().getTable("SmartDashboard").getEntry("Drive PID I");
+    driveD = NetworkTableInstance.getDefault().getTable("SmartDashboard").getEntry("Drive PID D");
+    turnP = NetworkTableInstance.getDefault().getTable("SmartDashboard").getEntry("Turn PID P");
+    turnI = NetworkTableInstance.getDefault().getTable("SmartDashboard").getEntry("Turn PID I");
+    turnD = NetworkTableInstance.getDefault().getTable("SmartDashboard").getEntry("Turn PID D");
+  
+    // Set default PID values
+    driveP.setDouble(swerveDrive.getModules()[0].getDrivePIDF().p);
+    driveI.setDouble(swerveDrive.getModules()[0].getDrivePIDF().i);
+    driveD.setDouble(swerveDrive.getModules()[0].getDrivePIDF().d);
+    turnP.setDouble(swerveDrive.getModules()[0].getAnglePIDF().p);
+    turnI.setDouble(swerveDrive.getModules()[0].getAnglePIDF().i);
+    turnD.setDouble(swerveDrive.getModules()[0].getAnglePIDF().d);
   }
 
   /**
@@ -63,5 +86,46 @@ public class SwerveSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
+    // Gyro data
+    SmartDashboard.putNumber("Gyro Angle (degrees)", swerveDrive.getYaw().getDegrees());
+    
+    // Turn encoders
+    SmartDashboard.putNumber("Front Left Turn Encoder (degrees)", swerveDrive.getModules()[0].getRelativePosition());
+    SmartDashboard.putNumber("Front Right Turn Encoder (degrees)", swerveDrive.getModules()[1].getRelativePosition());
+    SmartDashboard.putNumber("Back Left Turn Encoder (degrees)", swerveDrive.getModules()[2].getRelativePosition());
+    SmartDashboard.putNumber("Back Right Turn Encoder (degrees)", swerveDrive.getModules()[3].getRelativePosition());
+    
+    // Absolute encoders
+    SmartDashboard.putNumber("Front Left Absolute Encoder (degrees) [0, 360]", swerveDrive.getModules()[0].getAbsolutePosition());
+    SmartDashboard.putNumber("Front Right Absolute Encoder (degrees) [0, 360]", swerveDrive.getModules()[1].getAbsolutePosition());
+    SmartDashboard.putNumber("Rear Left Absolute Encoder (degrees) [0, 360]", swerveDrive.getModules()[2].getAbsolutePosition());
+    SmartDashboard.putNumber("Rear Right Absolute Encoder (degrees) [0, 360]", swerveDrive.getModules()[3].getAbsolutePosition());
+    
+    // PID values
+    double newDriveP = driveP.getDouble(swerveDrive.getModules()[0].getDrivePIDF().p);
+    double newDriveI = driveI.getDouble(swerveDrive.getModules()[0].getDrivePIDF().i);
+    double newDriveD = driveD.getDouble(swerveDrive.getModules()[0].getDrivePIDF().d);
+    double newTurnP = turnP.getDouble(swerveDrive.getModules()[0].getAnglePIDF().p);
+    double newTurnI = turnI.getDouble(swerveDrive.getModules()[0].getAnglePIDF().i);
+    double newTurnD = turnD.getDouble(swerveDrive.getModules()[0].getAnglePIDF().d);
+
+    PIDFConfig drivePID = new PIDFConfig(newDriveP, newDriveI, newDriveD);
+    PIDFConfig turnPID = new PIDFConfig(newTurnP, newTurnI, newTurnD);
+    
+    // Update PID controller values if changed
+    for (int i=0; i<4; i++)
+    {
+    swerveDrive.getModules()[i].setAnglePIDF(turnPID);
+    swerveDrive.getModules()[i].setDrivePIDF(drivePID);
+    }
+
+    // Log the PID values on the SmartDashboard
+    SmartDashboard.putNumber("Drive PID P", newDriveP);
+    SmartDashboard.putNumber("Drive PID I", newDriveI);
+    SmartDashboard.putNumber("Drive PID D", newDriveD);
+    SmartDashboard.putNumber("Turn PID P", newTurnP);
+    SmartDashboard.putNumber("Turn PID I", newTurnI);
+    SmartDashboard.putNumber("Turn PID D", newTurnD);
   }
 }
