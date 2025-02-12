242ba9d0578a2dac99f076b2241a0e09a47a05ef
andriibessarab <andriibessarab@gmail.com>
2025-02-12 09:45:23 -0500

    Make it so default drive command can either be w/ or w/o vision aim.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,14 +6,13 @@ package frc.robot;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.DriverJoystickConstants;
-import frc.robot.commands.AimWhileDriveCommand;
+import frc.robot.commands.TeleopDriveCommand;
 import frc.robot.subsystems.SwerveSubsystem;
 
 public class RobotContainer {
@@ -30,18 +29,14 @@ public class RobotContainer {
     m_DriverController = new CommandXboxController(DriverJoystickConstants.kDriverControllerPort);
 
     // Set default driving command
-    // Applies deadbands and inverts controls because joysticks
-    // are back-right positive while robot
-    // controls are front-left positive
-    // left stick controls translation
-    // right stick controls the angular velocity of the robot
     m_Swerve.setDefaultCommand(
-      m_Swerve.driveCommand(
-        () -> MathUtil.applyDeadband(m_DriverController.getLeftY() * -1, DriverJoystickConstants.kLeftXDeadband),
-        () -> MathUtil.applyDeadband(m_DriverController.getLeftX() * -1, DriverJoystickConstants.kLeftYDeadband),
-        () -> MathUtil.applyDeadband(m_DriverController.getRightX() * -1, DriverJoystickConstants.kRightXDeadband) * DriverJoystickConstants.kTurnMultiplier,
-        true,
-        false
+      new TeleopDriveCommand(
+        m_Swerve,
+        m_DriverController::getLeftY,
+        m_DriverController::getLeftX,
+        m_DriverController::getRightX,
+        m_DriverController.getHID()::getAButton,
+        () -> !m_DriverController.getHID().getBButton()
       )
     );
 
@@ -58,9 +53,6 @@ public class RobotContainer {
 
   private void configureBindings() {
     m_DriverController.a().onTrue((Commands.runOnce(m_Swerve::zeroGyro)));
-    m_DriverController.x().whileTrue(new AimWhileDriveCommand(m_Swerve, m_DriverController).repeatedly());
-    // m_DriverController.y().whileTrue(new AimAndGetInRangeCommand(m_Swerve, m_DriverController).repeatedly());
-
 
     // SysId Routines for Swerve
     // m_DriverController.x().onTrue(m_Swerve.sysIdDriveMotorCommand());
---
+++ src/main/java/frc/robot/commands/TeleopDriveCommand.java
@@ -0,0 +1,88 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import static frc.robot.Constants.VisionConstants.VISION_TURN_kP;
+import static frc.robot.Constants.VisionConstants.VISION_YAW_DEADBAND;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.DriverJoystickConstants;
+import frc.robot.subsystems.SwerveSubsystem;
+import frc.robot.subsystems.VisionSubsystem;
+
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
+public class TeleopDriveCommand extends Command {
+  private final SwerveSubsystem swerve;
+  private final VisionSubsystem vision;
+  private final DoubleSupplier forwardSupplier, strafeSupplier, turnSupplier;
+  private final BooleanSupplier fieldRelativeSupplier, visionAimSupplier;
+
+
+  /** Creates a new TeleopDriveCommand.
+   *
+   * @param swerve The SwerveSubsystem used by this command to drive the robot.
+   * @param forwardSupplier A DoubleSupplier that supplies the forward velocity of the robot.
+   * @param strafeSupplier A DoubleSupplier that supplies the strafe velocity of the robot.
+   * @param turnSupplier A DoubleSupplier that supplies the turn velocity of the robot.
+   * @param fieldRelativeSupplier A BooleanSupplier that supplies whether the robot should drive in field relative
+   * @param visionAimSupplier A BooleanSupplier that supplies whether the robot should aim using the vision system.
+   */
+  public TeleopDriveCommand(SwerveSubsystem swerve, DoubleSupplier forwardSupplier, DoubleSupplier strafeSupplier, DoubleSupplier turnSupplier, BooleanSupplier fieldRelativeSupplier, BooleanSupplier visionAimSupplier) {
+    this.swerve = swerve;
+    this.vision = swerve.getVisionSubsystem();
+    this.forwardSupplier = forwardSupplier;
+    this.strafeSupplier = strafeSupplier;
+    this.turnSupplier = turnSupplier;
+    this.fieldRelativeSupplier = fieldRelativeSupplier;
+    this.visionAimSupplier = visionAimSupplier;
+
+    addRequirements(this.swerve, this.vision);
+  }
+
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    // Get controller inputs
+    double forward = MathUtil.applyDeadband(forwardSupplier.getAsDouble() * -1, DriverJoystickConstants.kLeftXDeadband) * swerve.getMaximumDriveVelocity();
+    double strafe = MathUtil.applyDeadband(strafeSupplier.getAsDouble() * -1, DriverJoystickConstants.kLeftYDeadband) * swerve.getMaximumDriveVelocity();
+    double turn = MathUtil.applyDeadband(turnSupplier.getAsDouble() * -1, DriverJoystickConstants.kRightXDeadband) * DriverJoystickConstants.kTurnMultiplier * swerve.getMaximumAzimuthVelocity();
+    boolean fieldRelative = fieldRelativeSupplier.getAsBoolean();
+    boolean visionAim = visionAimSupplier.getAsBoolean();
+
+    // If visionAim is true, aim using the vision system
+    if (visionAim) {
+      var latestResultOptional = vision.getLatest2DResult();
+      if (latestResultOptional.isPresent()) {
+        var latestResult = latestResultOptional.get();
+          var target = latestResult.getBestTarget();
+          turn = -MathUtil.applyDeadband(target.getYaw(), VISION_YAW_DEADBAND) * VISION_TURN_kP * swerve.getMaximumAzimuthVelocity();
+        }
+      }
+
+    // Drive the robot
+    swerve.drive(forward, strafe, turn, fieldRelative, visionAim);
+  }
+
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
