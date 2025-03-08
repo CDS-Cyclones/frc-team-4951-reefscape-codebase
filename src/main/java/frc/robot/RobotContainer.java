b29b792c038a2e5573d9b98ff90cb8a538f1e2fe
Andrii Bessarab <andriibessarab@gmail.com>
2025-03-08 17:07:08 -0500

    Add drive command and few button bindings for AK swerve

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,79 +7,73 @@ package frc.robot;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.XboxController.Button;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-
-import frc.robot.commands.drive.TeleopDriveCommand;
-import frc.robot.commands.operation.manual.MoveElevatorManuallyCommand;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.commands.DriveCommands;
 import frc.robot.commands.operation.manual.MoveIntakeWheelsManuallyCommand;
 import frc.robot.commands.operation.manual.MovePivotManuallyCommand;
-import frc.robot.commands.operation.pid.ElevatorGoToCommand;
-import frc.robot.commands.operation.pid.PivotGoToCommand;
 import frc.robot.subsystems.manipulator.Elevator;
 import frc.robot.subsystems.manipulator.Pivot;
-import frc.robot.subsystems.manipulator.IntakeWheels;
 import frc.robot.subsystems.oi.OI;
-import frc.robot.subsystems.swerve.Swerve;
-import org.json.simple.parser.ParseException;
-
-import java.io.IOException;
+import frc.robot.subsystems.manipulator.IntakeWheels;
+import frc.robot.subsystems.swerve.Drive;
+import frc.robot.subsystems.swerve.GyroIO;
+import frc.robot.subsystems.swerve.GyroIOPigeon2;
+import frc.robot.subsystems.swerve.ModuleIO;
+import frc.robot.subsystems.swerve.ModuleIOSim;
+import frc.robot.subsystems.swerve.ModuleIOSpark;
+import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 public class RobotContainer {
-  private final Swerve m_swerve;
+  private final Drive swerve;
   private final Elevator m_elevator;
   private final Pivot m_pivot;
   private final IntakeWheels m_intakeWheels;
 
   // Autonomous command chooser
-  private final SendableChooser<Command> autoChooser;
+  private final LoggedDashboardChooser<Command> autoChooser;
 
-  // Whether the robot should drive in field relative mode or robot relative mode
-  private boolean fieldOriented = true;
+  public RobotContainer() {
+    switch (Constants.currentMode) {
+      case REAL:
+        // Real robot, instantiate hardware IO implementations
+        swerve = new Drive(new GyroIOPigeon2(), new ModuleIOSpark(0), new ModuleIOSpark(1), new ModuleIOSpark(2), new ModuleIOSpark(3));
+        break;
 
-  public RobotContainer() throws IOException, ParseException {
-    // Initialize subsystems
-    m_swerve = new Swerve();
+      case SIM:
+        // Sim robot, instantiate physics sim IO implementations
+        swerve = new Drive(new GyroIO() {}, new ModuleIOSim(), new ModuleIOSim(), new ModuleIOSim(), new ModuleIOSim());
+        break;
+
+      default:
+        // Replayed robot, disable IO implementations
+        swerve = new Drive(new GyroIO(){}, new ModuleIO(){}, new ModuleIO(){}, new ModuleIO(){}, new ModuleIO(){});
+        break;
+    }
+
+    // Set up PP auton routines
+    autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
+    
+    // Set up SysId routines
+    autoChooser.addOption("Drive Wheel Radius Characterization", DriveCommands.wheelRadiusCharacterization(swerve));
+    autoChooser.addOption("Drive Simple FF Characterization", DriveCommands.feedforwardCharacterization(swerve));
+    autoChooser.addOption("Drive SysId (Quasistatic Forward)", swerve.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
+    autoChooser.addOption("Drive SysId (Quasistatic Reverse)", swerve.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
+    autoChooser.addOption("Drive SysId (Dynamic Forward)", swerve.sysIdDynamic(SysIdRoutine.Direction.kForward));
+    autoChooser.addOption("Drive SysId (Dynamic Reverse)", swerve.sysIdDynamic(SysIdRoutine.Direction.kReverse));
+
     m_elevator = new Elevator();
     m_pivot = new Pivot();
     m_intakeWheels = new IntakeWheels();
 
-    // Set default driving commands
-    m_swerve.setDefaultCommand(
-      new TeleopDriveCommand(
-        m_swerve,
-        OI.m_driverController::getLeftY,
-        OI.m_driverController::getLeftX,
-        OI.m_driverController::getRightX,
-        () -> fieldOriented,
-        OI.m_driverController::getBButton
-      )
-    );
-
-    // m_elevator.setDefaultCommand(
-    //   new ElevatorGoToCommand(
-    //     m_elevator,
-    //       ManipulatorSubsystemsPositions.getCurrentElevatorPositionSupplier()
-    //   )
-    // );
-
-    m_pivot.setDefaultCommand(
-      new PivotGoToCommand(
-        m_pivot,
-        ManipulatorSubsystemsPositions.getCurrentPivotPositionSupplier()
-      )
-    );
-
-    // Auto chooser for selecting pathplanner trajectories
     registerNamedCommands();
-    autoChooser = AutoBuilder.buildAutoChooser();
-    SmartDashboard.putData("Auto Chooser", autoChooser);
-
     configureBindings();
   }
 
@@ -87,9 +81,29 @@ public class RobotContainer {
    * Configure button bindings.
    */
   private void configureBindings() {
-    new JoystickButton(OI.m_driverController, Button.kX.value).onTrue(new RunCommand(m_swerve::zeroHeading, m_swerve));
-    new JoystickButton(OI.m_driverController, Button.kY.value).whileTrue(new RunCommand(m_swerve::setX, m_swerve));
-    new JoystickButton(OI.m_driverController, Button.kB.value).onTrue(new RunCommand(() -> fieldOriented = !fieldOriented));;
+    // Default command, normal field-relative drive
+    swerve.setDefaultCommand(DriveCommands.joystickDrive(
+      swerve,
+      () -> -OI.m_driverController.getLeftY(),
+      () -> -OI.m_driverController.getLeftX(),
+      () -> -OI.m_driverController.getRightX())
+    );
+
+    // Lock to 0° when A button is held
+    new JoystickButton(OI.m_driverController, Button.kA.value).whileTrue(DriveCommands.joystickDriveAtAngle(
+      swerve,
+      () -> -OI.m_driverController.getLeftY(),
+      () -> -OI.m_driverController.getLeftX(),
+      () -> new Rotation2d())
+    );
+
+    // Switch to X pattern when X button is pressed
+    new JoystickButton(OI.m_driverController, Button.kX.value).onTrue(Commands.runOnce(swerve::stopWithX, swerve));
+
+    // Reset gyro to 0° when B button is pressed
+    new JoystickButton(OI.m_driverController, Button.kB.value).onTrue(Commands.runOnce(
+      () -> swerve.setPose(new Pose2d(swerve.getPose().getTranslation(), new Rotation2d())), swerve).ignoringDisable(true)
+    );
 
     new JoystickButton(OI.m_mainpulatorControllerManualBackup, Button.kY.value).onTrue(new RunCommand(() -> ManipulatorSubsystemsPositions.setCurrentPivotPosition(ManipulatorSubsystemsPositions.PivotPosition.IN)));
     // new JoystickButton(OI.m_mainpulatorControllerManualBackup, Button.kA.value).whileTrue(new MoveElevatorManuallyCommand(m_elevator, m_pivot, false));
@@ -127,6 +141,6 @@ public class RobotContainer {
    * @return the selected autonomous command
    */
   public Command getAutonomousCommand() {
-    return autoChooser.getSelected();
+    return autoChooser.get();
   }
 }
