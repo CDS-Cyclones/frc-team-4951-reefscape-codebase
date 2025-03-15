bbba7976a791a1714f39e90f7c4e1bd574e72cf7
Andrii Bessarab <andriibessarab@gmail.com>
2025-03-15 19:15:46 -0400

    Add end condition to pivot to position command

--- src/main/java/frc/robot/commands/pivot/PivotToPositionCommand.java
+++ src/main/java/frc/robot/commands/pivot/PivotToPositionCommand.java
@@ -70,6 +70,6 @@ public class PivotToPositionCommand extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return controller.atGoal();
   }
 }
